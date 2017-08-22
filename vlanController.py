f05c364550b7b6400b217ed06d33a5e12d37a248
Dimeji Fayomi <olafayomi@primus-lord.me>
2017-08-22 21:54:50 +1200

    Some commit

---
+++ vlanController.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import sys
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import set_ev_cls, MAIN_DISPATCHER
+from ryu.topology import switches, event
+from ryu.ofproto import ofproto_v1_3 as ofproto
+from ryu.lib.dpid import dpid_to_str, str_to_dpid
+from ryu.lib import hub
+from ryu import cfg
+import argparse
+# either take dpid from  terminal or read from yaml
+config_parser  = argparse.ArgumentParser(description='InterVLAN Routing Controller')
+
+
